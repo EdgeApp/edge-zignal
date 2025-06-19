d1894d7417e814c7ca867b17bbdaf544e2a6d84e
Jared <jared@edge.app>
2025-06-19 12:49:32 -0700

    Refactor: remove Signal UUID from custom fields, clean up parameters, add monikers.py for naming lists

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import time
 import json
 from signal_api import receive_messages, send_signal_message
-from zendesk import create_or_update_ticket, create_new_ticket
+from zendesk import create_or_update_ticket
 
 print("📬 Signal-to-Zendesk bridge started...")
 
 # Main loop to receive messages from Signal and create Zendesk tickets
 while True:
     messages = receive_messages()
-    
+
     if messages:
         print("📩 Raw Signal response:")
         print(json.dumps(messages, indent=2))
@@ -22,11 +22,11 @@ while True:
             data_msg = envelope.get("dataMessage", {})
             message_text = data_msg.get("message")
 
-            
+
 
             # Run if activity involves a non empty message from a user
             if source and message_text:
-                
+
                 # # Search for existing ticket by source UUID
                 # print(f"🔎 Checking for existing ticket for source: {source} ({sourceName})")
                 # existing_ticket = create_or_update_ticket(source, sourceName, f"[Signal Bridge] {message_text}", sourceUuid)
@@ -34,8 +34,8 @@ while True:
 
                 # If source is same as uuid, use sourceName instead
                 print(f"⭐️ New message from user: {sourceName}")
-                ticketNum = create_or_update_ticket(sourceNumber, sourceName, sourceUuid, f"[Signal Bridge] {message_text}") # !=> Remove [Signal Bridge] prefix after testing
-                
+                ticketNum = create_or_update_ticket(sourceUuid, f"[Signal Bridge] {message_text}") # !=> Remove [Signal Bridge] prefix after testing
+
                 print(f"📑 Ticket #{ticketNum}")
 
                 # Compose message for auto response to new ticket creation
@@ -50,10 +50,10 @@ while True:
                 if ticketNum != None:
                     print(f"📬 Sending new ticket confirmation message to {sourceName} - Ticket: {ticketNum}")
                     send_signal_message(sourceUuid, message)
-                    
+
                     # If new ticket created:
                     # Wait for 30 seconds before sending the next message to avoid race condition caused by limitation of Zendesk API
-                    time.sleep(30) 
+                    time.sleep(30)
             else:
                 print("Non-message event, skipping.")
     else:
